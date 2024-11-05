8d002dfb863b354870ffb335f0f15bb29b949439
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-11-05 13:40:48 +0100

    Release : Upload project to github

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex justify-center items-center h-[100dvh]">
+      <h1>Not ready yet</h1>
+    </div>
+    </>
+  );
+}
