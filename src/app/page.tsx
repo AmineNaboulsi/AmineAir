335139e56dd5ba85206a65b392770afa5a03250b
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-11-06 15:26:04 +0100

    Merging feature/search bar to devs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from './Components/header';
 import SearchBar from './Components/searchbar';
 import './globals.css'
-import Image from 'next/image'
 export default function Home() {
  
   return (
