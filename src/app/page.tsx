02d615f7796d50d2b8a7b58c8a1343581f65c9ca
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-11-06 15:24:41 +0100

    feature : Add Passagers popup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 import Header from './Components/header';
 import SearchBar from './Components/searchbar';
-
 import './globals.css'
-import Link from 'next/link';
-
+import Image from 'next/image'
 export default function Home() {
-
+ 
   return (
     <>
     <Header />
     <SearchBar />
-      Navigate to :
-      <ul>
-        <li><Link href="/searchresults">Search Results</Link></li>
-      </ul>
     </>
     
   );
