bb6b2d8dbecedf4b548e7ce7bfce57c7f1670b8c
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-11-06 16:07:23 +0100

    Fix : Addding booking dir

--- src/app/Booking/page.tsx
+++ src/app/Booking/page.tsx
@@ -7,6 +7,7 @@ function page() {
     <div>
       <Header />
       Booking page
+
       <div className="flex flex-col justify-center items-center">
       <Image width={100} height={100} src="/underM.gif" alt='' />
       Under maintenance
